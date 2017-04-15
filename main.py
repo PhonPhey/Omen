85b87dac693d819c1eb6b176a0471f36b18e4851
Unknown <kdv200330@bk.ru>
2017-04-15 22:20:44 +0300

    SLOMAL COD)))

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-''' Main module for omen engine '''
+
 #Author: Phonphey
 #Slaves: Oksovskii, KDV
+=======
+#super cod
+#DUP
+
+#Author: Phonphey
+#Slaves: Oksovskii, KDV
+====
+#super cod
+#DUP
+
+#Author: Phonphey
+#Slaves: Oksovskii, KDV
+ksovskii, KDV
+Oksovskii, KDV
+ksovskii, KDV
+ Oksovskii, KDV
+ksovskii, KDV
+Oksovskii, KDV
+ksovskii, KDV
+=======
+#Author: Phonphey
+#Slaves: Oksovskii, KDV
+=======
+#super cod
+#DUP
