2276314f6a392f114741757a1abfe1021171f945
phonphey <phonphey@o2.pl>
2017-04-19 20:37:09 +0300

    Cosm Edit

--- model.py
+++ model.py
@@ -1,6 +1,11 @@
 import peewee as pw
 
-main_db = pw.SqliteDatabase('db/main.db')
+try:
+    main_db = pw.SqliteDatabase('db/main.db')
+
+except peewee.OperationalError:
+    main_db_file = open('db/main.db', 'w+')
+    main_db = pw.SqliteDatabase('db/main.db')
 
 class players(pw.Model):
     name = pw.CharField()
@@ -12,15 +17,15 @@ class players(pw.Model):
         database = main_db
 
 class things(pw.Model):
-    name = pw.CharField() 
-    level = pw.IntegerField() 
-    strgh = pw.IntegerField() 
+    name = pw.CharField()
+    level = pw.IntegerField()
+    strgh = pw.IntegerField()
     json_meta_obj = pw.CharField()
 
     class Meta:
         database = main_db
 
-# DEBUG RUN 
-# players.create_table() 
-# things.create_table() 
+# DEBUG RUN
+players.create_table()
+things.create_table()
 # DEBUG STOP
