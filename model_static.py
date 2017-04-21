7c61bda23ff6bfdc655d08333c001d0843a338b0
oksovskii <21ololoha@mail.ru>
2017-04-21 20:59:57 +0300

    Commit with new rule for comment of code

--- model_static.py
+++ model_static.py
@@ -1,31 +1,16 @@
-'''Module consist static for Omen'''
-
 import os
+
 import peewee as pw
 
-'''
-Declaration of common variable:
-    name: name of object of [class_object]
-    json_meta_obj: json object of meta class
-    database: variable of meta classes
-'''
-
-# Trying connect to file DB
 try:
-    # Static DataBase object
     static_db = pw.SqliteDatabase('db/static.db')
 
-# Detecting error and correct
 except pw.OperationalError:
     os.makedirs('db/')
-    # Creating DataBase file
     static_db_file = open('db/static.db', 'w+')
-    # Static DataBase object
     static_db = pw.SqliteDatabase('db/static.db')
 
 class things(pw.Model):
-    '''class represents things'''
-
     name = pw.TextField()
     json_meta_obj = pw.TextField()
 
@@ -33,38 +18,32 @@ class things(pw.Model):
         database = static_db
 
 class monsters(pw.Model):
-    '''class represents monsters'''
-
     name = pw.TextField()
-    json_meta_obj = pw.TextField()
+    json_meta_obj = pw.TextField()       
 
     class Meta:
         database = static_db
 
 class npcs(pw.Model):
-    '''class represents npcs'''
-
     name = pw.TextField()
-    json_meta_obj = pw.TextField()
+    json_meta_obj = pw.TextField()       
 
     class Meta:
         database = static_db
 
 class events(pw.Model):
-    '''class represents events'''
-
     name = pw.TextField()
     json_obj = pw.TextField()
 
     class Meta:
         database = static_db
-
+        
 # DEBUG RUN
-#try:
- #  things.create_table()
-  # monsters.create_table()
-   #npcs.create_table()
-   #events.create_table()
-#except pw.OperationalError:
-#   pass
+try:
+   things.create_table()
+   monsters.create_table()
+   npcs.create_table()
+   events.create_table()
+except pw.OperationalError:
+    pass
 # DEBUG STOP
