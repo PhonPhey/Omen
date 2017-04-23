456ea7483914be03063ebb1c62b0d28387af00a3
oksovskii <21ololoha@gmail.com>
2017-04-23 14:27:02 +0300

    ANTI-POMPA BUG FIX

--- main.py
+++ main.py
@@ -1,44 +1,5 @@
-import peewee as pw
-import os
+''' Main module for omen engine '''
 
-try:
-    coor_db = pw.SqliteDatabase('db/coor.db')
-
-except pw.OperationalError:
-    os.makedirs('db/')
-    coor_db_file = open('db/coor.db', 'w+')
-    coor_db = pw.SqliteDatabase('db/coor.db')
-
-class npcs(pw.Model):
-    coordinate = pw.TextField()
-
-    class Meta:
-        database = coor_db
-
-class players(pw.Model):
-    coordinate = pw.TextField()
-
-    class Meta:
-        database = coor_db
-
-class things(pw.Model):
-    coordinate = pw.TextField()
-
-    class Meta:
-        database = coor_db
-
-class etc(pw.Model):
-    coordinate = pw.TextField()
-
-    class Meta:
-        database = coor_db
-
-# DEBUG RUN
-try:
-    npcs.create_table()
-    players.create_table()
-    things.create_table()
-    etc.create_table()
-except pw.OperationalError:
-    pass
-# DEBUG STOP
+# -*- coding: utf-8 -*-
+#Author: Phonphey
+#Slaves: Oksovskii, KDV
