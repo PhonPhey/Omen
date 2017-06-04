3ddc89ac709202263caddd35fa29066c7293d2bb
phonphey <phonphey@o2.pl>
2017-06-04 16:16:37 +0300

    rec

--- omen/__init__.py
+++ omen/__init__.py
@@ -1,12 +1,12 @@
 '''Init module'''
 
 from omen.header import *
-from omen.data_db import *
+from omen.models.data_db import *
 from peewee import  *
 
 def init_db():
     '''Function for inital db if it not inited'''
-    db  = SqliteDatabase("Omen/omen/"+DATA_DB)
+    db  = SqliteDatabase(DATA_DB)
     db.create_tables([Players, Inventory, Events, Things, Monsters, Npcs])
     
     
