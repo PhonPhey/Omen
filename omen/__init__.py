678abfb799218e78108c96b31c6771c24d7eabf1
phonphey <phonphey@o2.pl>
2017-06-05 20:29:34 +0300

    To pep8. To sha512

--- omen/__init__.py
+++ omen/__init__.py
@@ -6,17 +6,23 @@ from omen.header import *
 from omen.models.data_db import *
 from peewee import  *
 
-def init_db():
+def _init_db():
     '''Function for inital db if it not inited'''
+    
     db  = SqliteDatabase(DATA_DB)
     db.create_tables([Players, Inventory, Events, Things, Monsters, Npcs, Etc])
     
     
 def _check_db():
-    if SHA512_DB != "":
-        hash_md5 = hashlib.md5()
-        with open(DATA_DB, "rb") as f:
-            for chunk in iter(lambda: f.read(4096), b""):
-             hash_md5.update(chunk)
+        '''Function for check databse hash sum'''
+        
+        hash_sha512= hashlib.sha512()
+        file_db = open(DATA_DB, "rb")
+        
+        with open(HASH_SUM_PATH, "w+") as file_sum:
+            if file_sum.read(1) == "":
+                for chunk in iter(lambda: file_db.read(4096), b""):
+                    hash_sha512.update(chunk)
+                file_sum.write(hash_sha512.hexdigest())
             
-        print(hash_md5.hexdigest())
+        print("Check sha512 sum: ", hash_sha512.hexdigest())
--- omen/header.py
+++ omen/header.py
@@ -5,5 +5,4 @@ import os
 # PATHs
 DB_PATH = os.path.join(os.path.dirname(__file__), "db/")
 DATA_DB = os.path.join(os.path.dirname(__file__), "db/data.db")
-
-SHA512_DB = ""
+HASH_SUM_PATH = os.path.join(os.path.dirname(__file__), "db/dbsum.sha")
