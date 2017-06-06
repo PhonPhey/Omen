1f5ce45199b9b8759dc934ebe9caf386d004132f
kdv <kdv200330@bk.ru>
2017-06-06 21:06:38 +0300

    create player function by kdv, oksovskii, phonphey

--- omen/shell.py
+++ omen/shell.py
@@ -1,5 +1,21 @@
 ''' Shell module for Omen Engine '''
+from jsonsempai import *
 
-def create_hero():
+def _valid():
     
+
+def create_player():
+    player_name = input("Введите имя: ")
+    player_coordinate = [0,0]
+    point = 100
+    json_meta_obj = {"str": 0, "int": 0, "con": 0, "wis": 0, "char": 0, "dex": 0}
+    json_meta_obj["str"] = int(input("Введите силу: "))
+    json_meta_obj["int"] = int(input("Введите ловкость: "))
+    json_meta_obj["con"] = int(input("Введите телосложение: "))
+    json_meta_obj["wis"] = int(input("Введите интеллект: "))
+    json_meta_obj["char"] = int(input("Введите мудрость: "))
+    json_meta_obj["dex"] = int(input("Введите харизму: "))
+    
+
+
     
